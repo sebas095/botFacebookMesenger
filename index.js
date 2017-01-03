b3286848f821098de81dbbf91991398f92c2abd4
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-01-02 21:06:56 -0500

    webhook

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const logger = require('morgan');
 
+const APP_TOKEN = 'EAAKPZBQemFxoBAJlRszvrl1TCAK3UlQUC5r7YsVMbkELzMltjrrrUd7ECHPxQTHzJP74BjgF7emJ0KrOQ0Hh1eg6zxabXTNZCf4cCZAwDaVrcZAuQMz8oKBEX4yb2iBRHebCiNcAuEJ2tmiZCiEdo6ZCqyUZB1ZCxXn7K6mNmLPpzgZDZD';
+
 const app = express();
 const port = process.env.PORT || '3000';
 
@@ -15,4 +17,12 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
   res.send('Bienvenido al taller');
+});
+
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.verify_token'] === 'test_token_say_hello') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Tú no tiene que entrar aquí');
+  }
 });
